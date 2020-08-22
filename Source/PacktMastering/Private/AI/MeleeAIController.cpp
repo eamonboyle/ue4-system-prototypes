60b8a4c521220adac1818c99edac184448347386
Eamon Boyle <blaowskate@hotmail.com>
2020-08-22 23:18:03 +0100

    Add Paragon character and start Melee AI

---
+++ Source/PacktMastering/Private/AI/MeleeAIController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/MeleeAIController.h"
+
+AMeleeAIController::AMeleeAIController()
+{
+    HearingRadius = 300.f;
+}
